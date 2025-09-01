070f7ba89f22622e21fe9277e3373e1800dc430f
ElloRabyndra <Ellorabyndra123@gmail.com>
2025-09-01 22:54:30 +0700

    chore: setup starter folder structure and dependencies

---
+++ front-end/src/pages/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold  text-green-500">App</h1>
+      <Button>Button</Button>
+    </div>
+  );
+}
+
+export default App;
