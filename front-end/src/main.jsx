8f3e12fa556512705c2f839c30cf1fe26c61336d
ElloRabyndra <Ellorabyndra123@gmail.com>
2025-09-01 23:18:52 +0700

    feat: add auth and theme provider in main.jsx

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,13 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import { AuthProvider } from "./hooks/useAuth";
+import AuthRedirect from "./components/auth/AuthRedirect";
+import ToastWrapper from "./components/auth/ToastWrapper";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import ThemeProvider from "./context/ThemeContext.jsx";
 import App from "./pages/App";
 import "./style/Style.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <ThemeProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+          <ToastWrapper />
+        </BrowserRouter>
+      </ThemeProvider>
+    </AuthProvider>
   </StrictMode>
 );
--- front-end/src/pages/App.jsx
+++ front-end/src/pages/App.jsx
@@ -1,12 +1,23 @@
+import React, { useContext } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
 import { Button } from "@/components/ui/button";
+import ToggleButton from "@/components/theme/ToggleButton";
 
 const App = () => {
+  const { theme } = useContext(ThemeContext);
   return (
-    <div>
-      <h1 className="text-3xl font-bold  text-green-500">App</h1>
+    <div
+      className={`${theme} min-h-screen flex flex-col items-center justify-center gap-4`}
+    >
+      <div className="fixed top-4 right-4">
+        <ToggleButton />
+      </div>
+      <h1 className="text-3xl font-bold text-green-500 font-['Poppins']">
+        Tes
+      </h1>
       <Button>Button</Button>
     </div>
   );
-}
+};
 
 export default App;
