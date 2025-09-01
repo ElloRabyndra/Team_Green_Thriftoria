4056cdacad38f5269bd6589072d583b76189f9b2
ElloRabyndra <Ellorabyndra123@gmail.com>
2025-09-01 22:57:55 +0700

    feat: add theme context and toggle button

---
+++ front-end/src/components/theme/ToggleButton.jsx
@@ -0,0 +1,16 @@
+import { Switch } from "../ui/switch";
+import { useContext } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
+
+export default function ToggleButton() {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  const isDark = theme === "dark";
+  return (
+      <Switch
+        id="theme-toggle"
+        checked={isDark}
+        onCheckedChange={toggleTheme}
+        className={"scale-150 cursor-pointer"}
+      />
+  );
+}
