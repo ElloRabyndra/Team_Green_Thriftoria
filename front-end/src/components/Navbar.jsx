04914d8acbb96ca80be2e96843d2a8f2f1da8bb8
ElloRabyndra <Ellorabyndra123@gmail.com>
2025-09-02 10:55:17 +0700

    feat: add navbar and sidebar components

---
+++ front-end/src/components/Navbar.jsx
@@ -0,0 +1,96 @@
+import { Menu, Search, X } from "lucide-react";
+import ToggleButton from "./theme/ToggleButton";
+import { Card } from "./ui/card";
+import { Input } from "./ui/input";
+import { Link, useNavigate } from "react-router";
+
+export default function NavBar({
+  isMobileMenuOpen,
+  setIsMobileMenuOpen,
+  searchQuery,
+  setSearchQuery,
+  searchProducts,
+}) {
+  const navigate = useNavigate();
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    searchQuery.trim() && searchProducts(searchQuery);
+    navigate("/");
+    isMobileMenuOpen && setIsMobileMenuOpen(false);
+  };
+  return (
+    <Card className="p-0 block rounded-none border-none shadow-xs border-b sticky top-0 z-50">
+      <div className="mx-auto px-4">
+        <div className="flex justify-between items-center h-16">
+          {/* Logo/Brand */}
+          <div className="flex">
+            <Link to="/" className="text-2xl font-bold text-primary">
+              Thriftoria
+            </Link>
+          </div>
+
+          {/* Desktop Search Bar */}
+          <div className="hidden md:flex flex-1 max-w-lg mx-8">
+            <div className="w-full">
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Search className="h-5 w-5 text-gray-400" />
+                </div>
+                <form onSubmit={handleSearchSubmit}>
+                  <Input
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    className="pl-10"
+                    placeholder="Search for thrift products..."
+                  />
+                </form>
+              </div>
+            </div>
+          </div>
+
+          {/* Desktop Navigation */}
+          <div className="hidden md:flex items-center space-x-4">
+            {/* Theme Toggle */}
+            <div className="relative">
+              <ToggleButton />
+            </div>
+          </div>
+
+          {/* Mobile menu button */}
+          <div className="md:hidden flex items-center space-x-4">
+            <ToggleButton />
+            <button
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+              className="p-2 rounded-md text-primary hover:bg-secondary/80 focus:outline-none"
+            >
+              {isMobileMenuOpen ? (
+                <X className="h-6 w-6" />
+              ) : (
+                <Menu className="h-6 w-6" />
+              )}
+            </button>
+          </div>
+        </div>
+
+        {/* Mobile Search Bar */}
+        <div className="md:hidden pb-3">
+          <div>
+            <div className="relative">
+              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <Search className="h-5 w-5 text-gray-400" />
+              </div>
+              <form onSubmit={handleSearchSubmit}>
+                <Input
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                  className="pl-10"
+                  placeholder="Search for thrift products..."
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Card>
+  );
+}
---
+++ front-end/src/components/SideBar.jsx
@@ -0,0 +1,74 @@
+import { Link, useLocation } from "react-router";
+import { Card } from "./ui/card";
+import { ShoppingCart, User, Shirt, Footprints , Laptop } from "lucide-react";
+import { useAuth } from "@/hooks/useAuth";
+import { useProducts } from "@/hooks/useProducts";
+
+export default function ({isMobileMenuOpen, categories, changeCategories, cart} ) {
+  const { logout } = useAuth();
+  const location = useLocation();
+  const isActive = (path) => location.pathname === path;
+  const isSpecialPage = location.pathname === "/profile" || location.pathname === "/cart";
+  const {thriftCategories, shirtsCategories, shoesCategories, gadgetsCategories} = useProducts();
+  return (
+    <Card
+      className={`sidebar min-h-screen fixed z-10 right-0 bottom-0 top-16 mt-3 md:right-auto md:left-0 md:mt-0 shadow-xs border-none rounded-none w-60 border-r ${isMobileMenuOpen ? "sidebar-open" : "sidebar-close"}`}
+    >
+      <div className="py-6 px-4 md:py-0">
+        <div className="space-y-3 mb-4">
+          <h3 className="text-xs uppercase font-semibold mb-4 text-gray-500">
+            User
+          </h3>
+          <Link to="/profile" className={`${isActive("/profile") ? "bg-secondary/50 text-primary" : "hover:bg-secondary/50"} w-full flex items-center gap-3 p-3 rounded-lg transition-all duration-200 hover:bg-secondary/50 cursor-pointer`}>
+            <User className="h-5 w-5" />
+            <span>Profile</span>
+          </Link>
+          <Link to="/cart" className={`${isActive("/cart") ? "bg-secondary/50 text-primary" : "hover:bg-secondary/50"} relative w-full flex items-center gap-3 p-3 rounded-lg cursor-pointer`}>
+            <ShoppingCart className="h-5 w-5" />
+            <span>Cart</span>
+            <span className="absolute -top-1 left-6 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
+              {cart.reduce((total, item) => total + item.quantity, 0)}
+            </span>
+          </Link>
+        </div>
+
+        <div className="mb-4">
+          <h3 className="text-xs uppercase font-semibold mb-4 text-gray-500">
+            Categories
+          </h3>
+          <div className="space-y-2">
+            <Link to="/" onClick={() => changeCategories(thriftCategories)} className={`${!isSpecialPage && (categories === thriftCategories || categories.length === 0 ) ? "bg-secondary/50 text-primary" : "hover:bg-secondary/50"} w-full flex items-center gap-3 p-3 rounded-lg cursor-pointer`}>
+              <i className="bx bx-grid-alt text-2xl"></i>
+              <span>All</span>
+            </Link>
+            <Link to="/" onClick={() => changeCategories(shirtsCategories)} className={`${!isSpecialPage && categories === shirtsCategories ? "bg-secondary/50 text-primary" : "hover:bg-secondary/50"} w-full flex items-center gap-3 p-3 rounded-lg cursor-pointer`}>
+              <Shirt className="h-5 w-5" />
+              <span>Shirts</span>
+            </Link>
+            <Link to="/" onClick={() => changeCategories(shoesCategories)} className={`${!isSpecialPage && categories === shoesCategories ? "bg-secondary/50 text-primary" : "hover:bg-secondary/50"} w-full flex items-center gap-3 p-3 rounded-lg cursor-pointer`}>
+              <Footprints className="h-5 w-5" />
+              <span>Shoes</span>
+            </Link>
+            <Link to="/" onClick={() => changeCategories(gadgetsCategories)} className={`${!isSpecialPage && categories === gadgetsCategories ? "bg-secondary/50 text-primary" : "hover:bg-secondary/50"} w-full flex items-center gap-3 p-3 rounded-lg cursor-pointer`}>
+              <Laptop className="h-5 w-5" />
+              <span>Gadget</span>
+            </Link>
+          </div>
+        </div>
+
+        <div className="space-y-3">
+          <h3 className="text-xs uppercase font-semibold mb-4 text-gray-500">
+            Preference
+          </h3>
+          <button
+            onClick={logout}
+            className="w-full flex items-center gap-3 p-3 rounded-lg text-white bg-red-500 hover:bg-red-600 transition-all duration-200 cursor-pointer"
+          >
+            <i className="bx bx-log-out text-2xl"></i>
+            <span>Logout</span>
+          </button>
+        </div>
+      </div>
+    </Card>
+  );
+}
