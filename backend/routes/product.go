288dfc1d4478e1023ca987bf26c3a909628e5cb0
Steven <stevenbae9@gmail.com>
2025-09-18 00:13:49 +0700

    feat(product): make CRUD handler feature for product endpoint

--- backend/routes/product.go
+++ backend/routes/product.go
@@ -1,12 +1,15 @@
 package routes
 
 import (
+	"finpro/controllers"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func ProductsRoutes(api fiber.Router){
 	event := api.Group("/products")
-	event.Get("/", func (c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	event.Get("/", controllers.GetProducts)
+	event.Post("/", controllers.CreateProduct)
+	event.Patch("/:id", controllers.UpdateProduct)
+	event.Delete("/:id", controllers.DeleteProduct)
 }
