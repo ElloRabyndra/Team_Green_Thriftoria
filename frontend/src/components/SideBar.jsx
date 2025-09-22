87cc42772df7f317ac1f5578b5277d717162490b
ElloRabyndra <Ellorabyndra123@gmail.com>
2025-09-22 13:47:41 +0700

    feat: update sidebar with authorization view

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,15 +1,21 @@
 import { Link, useLocation } from "react-router";
 import { Card } from "./ui/card";
-import { ShoppingCart, User, Shirt, Footprints , Laptop } from "lucide-react";
+import { ShoppingCart, User, Shirt, Footprints , LayoutGrid , ShoppingBag  } from "lucide-react";
 import { useAuth } from "@/hooks/useAuth";
 import { useProducts } from "@/hooks/useProducts";
+import { useEffect } from "react";
 
-export default function ({isMobileMenuOpen, categories, changeCategories, cart} ) {
-  const { logout } = useAuth();
+export default function ({isMobileMenuOpen, categories, changeCategories, cart}) {
+  const { user, logout, isLoading } = useAuth();
   const location = useLocation();
-  const isActive = (path) => location.pathname === path;
-  const isSpecialPage = location.pathname === "/profile" || location.pathname === "/cart";
+  const isActive = (path) => location.pathname.startsWith(path);
+  const isSpecialPage = location.pathname === "/profile" || location.pathname === "/cart" || location.pathname.startsWith("/dashboard");
   const {thriftCategories, shirtsCategories, shoesCategories, gadgetsCategories} = useProducts();
+
+  useEffect(() => {
+    if(!isLoading && !user) logout();
+  }, [user, isLoading]);
+
   return (
     <Card
       className={`sidebar min-h-screen fixed z-10 right-0 bottom-0 top-16 mt-3 md:right-auto md:left-0 md:mt-0 shadow-xs border-none rounded-none w-60 border-r ${isMobileMenuOpen ? "sidebar-open" : "sidebar-close"}`}
@@ -23,13 +29,20 @@ export default function ({isMobileMenuOpen, categories, changeCategories, cart} 
             <User className="h-5 w-5" />
             <span>Profile</span>
           </Link>
-          <Link to="/cart" className={`${isActive("/cart") ? "bg-secondary/50 text-primary" : "hover:bg-secondary/50"} relative w-full flex items-center gap-3 p-3 rounded-lg cursor-pointer`}>
-            <ShoppingCart className="h-5 w-5" />
-            <span>Cart</span>
-            <span className="absolute -top-1 left-6 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
-              {cart.reduce((total, item) => total + item.quantity, 0)}
-            </span>
+          <Link to="/dashboard" className={`${isActive("/dashboard") ? "bg-secondary/50 text-primary" : "hover:bg-secondary/50"} w-full flex items-center gap-3 p-3 rounded-lg transition-all duration-200 hover:bg-secondary/50 cursor-pointer`}>
+            <LayoutGrid className="h-5 w-5" />
+            <span>Dashboard</span>
           </Link>
+          {/* Cek role */}
+          {(user?.userRole === "buyer" || user?.userRole === "seller") && (
+            <Link to="/cart" className={`${isActive("/cart") ? "bg-secondary/50 text-primary" : "hover:bg-secondary/50"} relative w-full flex items-center gap-3 p-3 rounded-lg cursor-pointer`}>
+              <ShoppingCart className="h-5 w-5" />
+              <span>Cart</span>
+              <span className="absolute -top-1 left-6 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
+                {cart.reduce((total, item) => total + item.quantity, 0)}
+              </span>
+            </Link>
+          )}
         </div>
 
         <div className="mb-4">
@@ -38,7 +51,7 @@ export default function ({isMobileMenuOpen, categories, changeCategories, cart} 
           </h3>
           <div className="space-y-2">
             <Link to="/" onClick={() => changeCategories(thriftCategories)} className={`${!isSpecialPage && (categories === thriftCategories || categories.length === 0 ) ? "bg-secondary/50 text-primary" : "hover:bg-secondary/50"} w-full flex items-center gap-3 p-3 rounded-lg cursor-pointer`}>
-              <i className="bx bx-grid-alt text-2xl"></i>
+              <ShoppingBag className="h-5 w-5" />
               <span>All</span>
             </Link>
             <Link to="/" onClick={() => changeCategories(shirtsCategories)} className={`${!isSpecialPage && categories === shirtsCategories ? "bg-secondary/50 text-primary" : "hover:bg-secondary/50"} w-full flex items-center gap-3 p-3 rounded-lg cursor-pointer`}>
@@ -48,10 +61,6 @@ export default function ({isMobileMenuOpen, categories, changeCategories, cart} 
             <Link to="/" onClick={() => changeCategories(shoesCategories)} className={`${!isSpecialPage && categories === shoesCategories ? "bg-secondary/50 text-primary" : "hover:bg-secondary/50"} w-full flex items-center gap-3 p-3 rounded-lg cursor-pointer`}>
               <Footprints className="h-5 w-5" />
               <span>Shoes</span>
-            </Link>
-            <Link to="/" onClick={() => changeCategories(gadgetsCategories)} className={`${!isSpecialPage && categories === gadgetsCategories ? "bg-secondary/50 text-primary" : "hover:bg-secondary/50"} w-full flex items-center gap-3 p-3 rounded-lg cursor-pointer`}>
-              <Laptop className="h-5 w-5" />
-              <span>Gadget</span>
             </Link>
           </div>
         </div>
