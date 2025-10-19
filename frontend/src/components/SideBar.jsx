1f93da70d1c51b25a5073285d0925090bfe5e3bf
ElloRabyndra <Ellorabyndra123@gmail.com>
2025-10-20 00:23:58 +0700

    feat: update sidebar and routing

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,41 +1,86 @@
 import { Link, useLocation } from "react-router";
 import { Card } from "./ui/card";
-import { ShoppingCart, User, Shirt, Box , LayoutGrid , ShoppingBag  } from "lucide-react";
+import {
+  ShoppingCart,
+  User,
+  Shirt,
+  Box,
+  LayoutGrid,
+  ShoppingBag,
+} from "lucide-react";
 import { useAuth } from "@/hooks/useAuth";
 import { useProducts } from "@/hooks/useProducts";
 import { useEffect } from "react";
 
-export default function ({isMobileMenuOpen, categories, changeCategories, cart}) {
+export default function ({
+  isMobileMenuOpen,
+  categories,
+  changeCategories,
+  cart,
+}) {
   const { user, logout, isLoading } = useAuth();
   const location = useLocation();
   const isActive = (path) => location.pathname.startsWith(path);
-  const isSpecialPage = location.pathname === "/profile" || location.pathname === "/cart" || location.pathname.startsWith("/dashboard");
-  const {thriftCategories, shirtsCategories, shoesCategories, gadgetsCategories} = useProducts();
+  const isSpecialPage =
+    location.pathname === "/profile" ||
+    location.pathname === "/cart" ||
+    location.pathname === "/checkout" ||
+    location.pathname.startsWith("/dashboard") ||
+    location.pathname.startsWith("/shop");
+  const {
+    thriftCategories,
+    shirtsCategories,
+    shoesCategories,
+    gadgetsCategories,
+  } = useProducts();
 
   useEffect(() => {
-    if(!isLoading && !user) logout();
+    if (!isLoading && !user) logout();
   }, [user, isLoading]);
 
   return (
     <Card
-      className={`sidebar min-h-screen fixed z-10 right-0 bottom-0 top-16 mt-3 md:right-auto md:left-0 md:mt-0 shadow-xs border-none rounded-none w-60 border-r ${isMobileMenuOpen ? "sidebar-open" : "sidebar-close"}`}
+      className={`sidebar min-h-screen fixed z-10 right-0 bottom-0 top-16 mt-3 md:right-auto md:left-0 md:mt-0 shadow-xs border-none rounded-none w-60 border-r ${
+        isMobileMenuOpen ? "sidebar-open" : "sidebar-close"
+      }`}
     >
       <div className="py-6 px-4 md:py-0">
         <div className="space-y-3 mb-4">
           <h3 className="text-xs uppercase font-semibold mb-4 text-gray-500">
             User
           </h3>
-          <Link to="/profile" className={`${isActive("/profile") ? "bg-secondary/50 text-primary" : "hover:bg-secondary/50"} w-full flex items-center gap-3 p-3 rounded-lg transition-all duration-200 hover:bg-secondary/50 cursor-pointer`}>
+          <Link
+            to="/profile"
+            className={`${
+              (isActive("/profile") || isActive("/shop"))
+                ? "bg-secondary/50 text-primary"
+                : "hover:bg-secondary/50"
+            } w-full flex items-center gap-3 p-3 rounded-lg transition-all duration-200 hover:bg-secondary/50 cursor-pointer`}
+          >
             <User className="h-5 w-5" />
             <span>Profile</span>
           </Link>
-          <Link to="/dashboard" className={`${isActive("/dashboard") ? "bg-secondary/50 text-primary" : "hover:bg-secondary/50"} w-full flex items-center gap-3 p-3 rounded-lg transition-all duration-200 hover:bg-secondary/50 cursor-pointer`}>
+          <Link
+            to="/dashboard"
+            className={`${
+              isActive("/dashboard")
+                ? "bg-secondary/50 text-primary"
+                : "hover:bg-secondary/50"
+            } w-full flex items-center gap-3 p-3 rounded-lg transition-all duration-200 hover:bg-secondary/50 cursor-pointer`}
+          >
             <LayoutGrid className="h-5 w-5" />
             <span>Dashboard</span>
           </Link>
           {/* Cek role */}
           {(user?.role === "buyer" || user?.role === "seller") && (
-            <Link to="/cart" className={`${isActive("/cart") ? "bg-secondary/50 text-primary" : "hover:bg-secondary/50"} relative w-full flex items-center gap-3 p-3 rounded-lg cursor-pointer`}>
+            <Link
+              to="/cart"
+              className={`${
+                isActive("/cart") || isActive("/checkout")
+                  ? "bg-secondary/50 text-primary"
+                  : "hover:bg-secondary/50"
+              } relative w-full flex items-center gap-3 p-3 rounded-lg cursor-pointer`}
+            >
               <ShoppingCart className="h-5 w-5" />
               <span>Cart</span>
               <span className="absolute -top-1 left-6 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
@@ -50,15 +95,40 @@ export default function ({isMobileMenuOpen, categories, changeCategories, cart})
             Categories
           </h3>
           <div className="space-y-2">
-            <Link to="/" onClick={() => changeCategories(thriftCategories)} className={`${!isSpecialPage && (categories === thriftCategories || categories.length === 0 ) ? "bg-secondary/50 text-primary" : "hover:bg-secondary/50"} w-full flex items-center gap-3 p-3 rounded-lg cursor-pointer`}>
+            <Link
+              to="/"
+              onClick={() => changeCategories(thriftCategories)}
+              className={`${
+                !isSpecialPage &&
+                (categories === thriftCategories || categories.length === 0)
+                  ? "bg-secondary/50 text-primary"
+                  : "hover:bg-secondary/50"
+              } w-full flex items-center gap-3 p-3 rounded-lg cursor-pointer`}
+            >
               <ShoppingBag className="h-5 w-5" />
               <span>All</span>
             </Link>
-            <Link to="/" onClick={() => changeCategories(shirtsCategories)} className={`${!isSpecialPage && categories === shirtsCategories ? "bg-secondary/50 text-primary" : "hover:bg-secondary/50"} w-full flex items-center gap-3 p-3 rounded-lg cursor-pointer`}>
+            <Link
+              to="/"
+              onClick={() => changeCategories(shirtsCategories)}
+              className={`${
+                !isSpecialPage && categories === shirtsCategories
+                  ? "bg-secondary/50 text-primary"
+                  : "hover:bg-secondary/50"
+              } w-full flex items-center gap-3 p-3 rounded-lg cursor-pointer`}
+            >
               <Shirt className="h-5 w-5" />
               <span>Fashion</span>
             </Link>
-            <Link to="/" onClick={() => changeCategories(shoesCategories)} className={`${!isSpecialPage && categories === shoesCategories ? "bg-secondary/50 text-primary" : "hover:bg-secondary/50"} w-full flex items-center gap-3 p-3 rounded-lg cursor-pointer`}>
+            <Link
+              to="/"
+              onClick={() => changeCategories(shoesCategories)}
+              className={`${
+                !isSpecialPage && categories === shoesCategories
+                  ? "bg-secondary/50 text-primary"
+                  : "hover:bg-secondary/50"
+              } w-full flex items-center gap-3 p-3 rounded-lg cursor-pointer`}
+            >
               <Box className="h-5 w-5" />
               <span>Others</span>
             </Link>
